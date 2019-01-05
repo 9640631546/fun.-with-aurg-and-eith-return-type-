c6a3f716147965afe479e5810d2166af405f6992
9640631546 <46022794+9640631546@users.noreply.github.com>
2019-01-05 16:49:03 +0530

    Add files via upload

---
+++ MLKMLK.C
@@ -0,0 +1,20 @@
+//function with args with return value
+#include<stdio.h>
+#include<conio.h>
+int sum(int,int);
+
+void main()
+{
+	int num1,num2,ret;
+	clrscr();
+	printf("enter num1 and num2 values");
+	scanf("%d%d",&num1,&num2);
+	ret = sum(num1,num2);
+	printf("sum of %d and %d is %d",num1,num2,ret);
+
+getch();
+}
+int sum(int num1,int num2)
+{
+	return num1+num2;
+}
